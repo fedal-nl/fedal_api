6342bc944b51c0bbd884626bb2b75c87636e8c9d
Omar Aljazairy <onha2001@gmail.com>
2023-11-27 23:12:17 +0100

    #4 adjusted the settings for postgreql

--- api/test_settings.py
+++ api/test_settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'NAME': os.environ.get("POSTGRES_DB", "Spanglish"),
         'USER': os.environ.get("POSTGRES_USER", "postgres"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        'HOST': os.environ.get("POSTGRES_HOST", "postgres"),
+        'HOST': os.environ.get("POSTGRES_HOST", "127.0.0.1"),
         'PORT': os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
