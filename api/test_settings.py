b468a0ad339baee4ee1fc857d217150d1f766060
Omar Aljazairy <onha2001@gmail.com>
2023-11-27 22:50:36 +0100

    #4 adjusted the settings for postgreql

--- api/test_settings.py
+++ api/test_settings.py
@@ -107,10 +107,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("POSTGRES_DB", "Spanglish"),
-        'USER': os.environ.get("POSTGRES_USER", "testuser"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "testpassword"),
+        'USER': os.environ.get("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "postgres"),
         'HOST': os.environ.get("POSTGRES_HOST", "postgres"),
-        'PORT': os.environ.get("POSTGRES_PORT", ""),
+        'PORT': os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
